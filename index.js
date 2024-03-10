81011d95f7c26e114ec9af526d27144e0dcb9da5
18120528 <57126456+18120528@users.noreply.github.com>
2024-03-10 21:24:44 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -27,11 +27,21 @@ async function sendHTML(path,res)
 //GET
 app.get('/',async (req,res)=>
 {
-    await sendHTML('./home.html',res)
+    await sendHTML('./home.html',res);
 });
-app.get('/quang',async (req,res)=>
+app.get('/user',async (req,res)=>
 {
-    await sendHTML('./quang.html',res)
+    await sendHTML('./user.html',res);
+});
+const IDlist = {1: 'quang', 2:'quynh'};
+async function sendAtribute(id,res)
+{
+    await res.status(200).send(IDlist[id])
+}
+app.get('/user/:id',async (req,res)=>
+{
+    const{id}=req.params
+    await sendAtribute(id,res);
 });
 //POST
 
