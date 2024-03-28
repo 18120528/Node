df05015ebd0763ad3ff68e199cb8e110ec719d2a
18120528 <57126456+18120528@users.noreply.github.com>
2024-03-28 23:20:45 +0700

    Update users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -9,6 +9,10 @@ router.get('/',async (req,res)=>
     //res.status(200).send(await fs.readFile('resource/home.html','utf-8'));
     await res.status(200).json([{'id':1,'name':'quang'}]);
 });
-
+router.post('/',async (req,res)=>
+{
+    //res.status(200).send(await fs.readFile('resource/home.html','utf-8'));
+    await res.status(200).json([{'id':1,'name':'quang'}]);
+});
 //export this module
 module.exports=router;
