abff0f6bbe43e222460451500e4f1c4678ed4bdb
18120528 <57126456+18120528@users.noreply.github.com>
2024-04-24 13:01:45 +0700

    fix conflict

--- src/models/activeSession.js
+++ src/models/activeSession.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-<<<<<<< HEAD
+//
 const Schema = mongoose.Schema;
 
 const sessionSchema = new Schema(
@@ -7,18 +7,7 @@ const sessionSchema = new Schema(
         userID: { type: String, required: true },
         sessionID: { type: String, required: true },
         expiresAt: { type: Date, default: Date.now() + 2 * 60 * 60 * 1000 } // 2 hours in milliseconds
-=======
-
-const sessionSchema = new mongoose.Schema(
-    {
-        userID: { type: String, required: true },
-        sessionID: { type: String, required: true },
-    }, 
-    {
-        expires: 7200, // 2 hours * 60 minutes * 60 seconds
->>>>>>> 2266049793faa4274b6f5eb7cbc86f64467c3885
     });
-
 const Session = mongoose.model('active_sessions', sessionSchema);
 
 module.exports = Session;
